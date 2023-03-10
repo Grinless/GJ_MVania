8a8d1d3d13554091469d321a128795ee333d7ee0
Cat Ater <103862724@student.swin.edu.au>
2023-03-11 00:54:23 +1100

    Updated the bat AI controller.
    Added AI logic to the Bat Enemy.

--- Assets/Scripts/Components/BatController.cs
+++ Assets/Scripts/Components/BatController.cs
@@ -5,113 +5,58 @@ using UnityEngine;
 [System.Serializable]
 public class BatData
 {
-    public float distance = -1;
-    public float width = 1;
-    public float offset = -1;
-    public float initalAngle = 0;
-    public float maxAngle = 30;
-    public int numberOfRays = 2;
+    public float batHealth;
+    public float batSpeed;
+    public BoxCollider2D collisionObject;
+    public Rigidbody2D body2D;
 }
 
 public class BatController : MonoBehaviour
 {
-    ConeCaster _coneCaster;
 
     public BatData data = new BatData();
+    public Vector2 trackedPosition;
+    [SerializeField]private bool active = false;
 
-    //public float distance = -1;
-    //public float width = 1;
-    //public float offset = -1;
-    //public float initalAngle = 0;
-    //public float maxAngle = 30;
-    //public int numberOfRays = 2;
-
-    //public Vector2 Position
-    //{
-    //    get => new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + offset);
-    //}
-
-    //public float InitalAngleRadians
-    //{
-    //    get => initalAngle * Mathf.Deg2Rad;
-    //}
-
-    //public float MaxAngleRadians
-    //{
-    //    get => maxAngle * Mathf.Deg2Rad;
-    //}
-
-    void Start()
-    {
-        //Calculate Rays.
-        //GenerateRays();
-
-        _coneCaster = new ConeCaster(ref data, transform);
-    }
-
-    //private void GenerateRays()
-    //{
-    //    List<VectorContainer> list = new List<VectorContainer>();
-    //    float angleIncrement = MaxAngleRadians / (numberOfRays * 2);
-    //    float currentAngle = (0 * Mathf.Deg2Rad) + angleIncrement;
-    //    float currentAngleNeg = (0 * Mathf.Deg2Rad) - angleIncrement;
-
-
-    //    for (int i = 0; i < numberOfRays; i++)
-    //    {
-
-    //        list.Add(CreateRayVecs(Position, currentAngle));
-    //        list.Add(CreateRayVecs(Position, currentAngleNeg));
-    //        currentAngle += angleIncrement;
-    //        currentAngleNeg -= angleIncrement;
-    //    }
-
-    //    rays = list.ToArray();
-    //}
-
-    //private VectorContainer CreateRayVecs(Vector2 position, float radian)
-    //{
-    //    return new VectorContainer(
-    //                position,
-    //                new Vector2(
-    //                    position.x + Mathf.Cos(InitalAngleRadians + radian) * Mathf.Rad2Deg,
-    //                    position.y + Mathf.Sin(InitalAngleRadians + radian) * Mathf.Rad2Deg
-    //                    ).normalized);
-    //}
 
     void Update()
     {
-        _coneCaster.CheckCollision();
+        if (!active)
+            return;
+
+        data.body2D.velocity = Vector2.down * data.batSpeed * Time.deltaTime;
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Triggered Detection: " + collision.gameObject.name);
+
+        if (collision.gameObject.layer != 0)
+        {
+            active = true;
+            data.body2D.gravityScale = 1;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!active)
+            return;
+
+        if (collision != null)
+        {
+            if (collision.gameObject.GetComponent<PlayerController>())
+            {
+                Debug.Log("TODO: Update this to apply damage to the player!!!!!");
+            }
+
+            gameObject.SetActive(false);
+        }
     }
 
     private void OnDrawGizmos()
     {
-        if (_coneCaster == null)
-        {
-            _coneCaster = new ConeCaster(ref data, transform);
-        }
-
-        //_coneCaster.SetInitalAngle(initalAngle);
-
-        _coneCaster.OnGizmosDraw();
-
-        //GenerateRays();
-
-        //Gizmos.color = Color.white;
-
-        //foreach (VectorContainer item in rays)
-        //{
-        //    Gizmos.DrawRay(new Ray(item.Start, item.End * distance));
-        //}
-
-        //Gizmos.color = Color.red;
-
-        //Gizmos.DrawLine(rays[0].Start, rays[0].End);
-        //Gizmos.DrawLine(rays[rays.Length -1].Start, rays[rays.Length -1].End);
-    }
-
-    bool CheckForPlayer()
-    {
-        return false;
+        Gizmos.DrawWireSphere(trackedPosition, 0.2f);
     }
 }
