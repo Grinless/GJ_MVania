c15c66c7fac0e28e887046f6a8087b88cb0c998c
Cat Ater <103862724@student.swin.edu.au>
2023-03-14 06:58:49 +1100

    Added Chase AI and Path.

---
+++ Assets/Scripts/Components/Boss/Boss_MassChase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boss_MassData
+{
+    public float health;
+    public float speed;
+    public float smoothing = 0.01f;
+    public float acceptibleDistX = 0.5f;
+    public float acceptibleDistY = 0.5f;
+    public bool killable;
+    public bool active;
+}
+
+public class Boss_MassChase : MonoBehaviour
+{
+    public WaypointSystem waypointsSystem;
+    public Boss_MassData data;
+
+    private void Start() => waypointsSystem.Setup(gameObject);
+
+    private void Update() => waypointsSystem.UpdateWaypoints(gameObject.transform.position);
+
+    private void LateUpdate() => waypointsSystem.LateUpdate(gameObject, data.speed);
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(gameObject.layer == 6)
+            ((IPlayerDamage)PlayerController.instance).ApplyDamage(5);
+    }
+
+    #region Draw Points
+    private void OnDrawGizmosSelected() => waypointsSystem.DrawGizmos();
+    #endregion
+}
---
+++ Assets/Scripts/Components/Boss/WaypointSystem.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class WayPoint
+{
+    public Vector2 Position = new Vector2();
+    public float delayTime;
+    public bool delay;
+}
+
+[System.Serializable]
+public class WaypointSystem
+{
+    private int index = 1;
+    public WayPoint[] waypoints;
+    public Vector2 target;
+    public bool active = false; 
+
+    public void Setup(GameObject obj)
+    {
+        obj.transform.position = waypoints[0].Position;
+        target = waypoints[index].Position;
+    }
+
+    public void UpdateWaypoints(Vector2 currentPosition) => UpdatePoint(currentPosition);
+
+    public void LateUpdate(GameObject obj, float speed)
+    {
+        if (!active)
+            return;
+
+        obj.transform.position = 
+            Vector3.MoveTowards(obj.transform.position, target, speed * Time.deltaTime);
+    }
+
+    private void UpdatePoint(Vector2 currentPosition)
+    {
+        if (!active)
+            return;
+
+        if ((currentPosition == target && index < waypoints.Length - 1))
+        {
+            index++;
+            target = waypoints[index].Position;
+        }
+    }
+
+    #region Draw Points
+    public void DrawGizmos()
+    {
+        if(waypoints.Length == 0) 
+            return;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(waypoints[0].Position, 0.3f);
+
+        Gizmos.color = Color.red;
+
+        for (int i = 1; i < waypoints.Length; i++)
+        {
+            Gizmos.DrawSphere(waypoints[i].Position, 0.3f);
+        }
+
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(waypoints[i].Position, waypoints[i + 1].Position);
+        }
+    }
+
+    #endregion
+}
